e205c1b321e156aac71c51e9a95dfc734738e324
ClaytonS2013 <ClaytonS2013@gmail.com>
2025-11-08 07:34:44 -0600

    Update __init__.py

--- sheets_sync/__init__.py
+++ sheets_sync/__init__.py
@@ -1,16 +1,1 @@
-"""
-Sheets Sync Module - Package Initialization
-This makes sheets_sync a proper Python package
-"""
-
-from .sync import (
-    sync_sheets_to_supabase,
-    sync_supabase_to_sheets
-)
-
-__all__ = [
-    'sync_sheets_to_supabase',
-    'sync_supabase_to_sheets'
-]
-
-__version__ = '1.0.0'
+# Empty file to make sheets_sync a Python package
