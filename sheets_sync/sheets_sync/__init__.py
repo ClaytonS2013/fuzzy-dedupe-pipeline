3cdaddf0c783e904cbf295a7c1953057e495900f
ClaytonS2013 <ClaytonS2013@gmail.com>
2025-11-07 18:07:21 -0600

    Create __init__.py

---
+++ sheets_sync/sheets_sync/__init__.py
@@ -0,0 +1,9 @@
+"""
+   sheets_sync package
+
+   Bidirectional sync between Google Sheets and Supabase
+   """
+
+   from .main import sync_sheets_to_supabase, write_clean_data_to_sheet
+
+   __all__ = ['sync_sheets_to_supabase', 'write_clean_data_to_sheet']
