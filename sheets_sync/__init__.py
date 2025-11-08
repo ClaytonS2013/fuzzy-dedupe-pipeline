7bfa277056009a4797f77ec01182d6fb68cc4ce8
ClaytonS2013 <ClaytonS2013@gmail.com>
2025-11-08 08:03:48 -0600

    Update __init__.py

--- sheets_sync/__init__.py
+++ sheets_sync/__init__.py
@@ -1,9 +1,1 @@
-# CRITICAL FIXES TO RUN:
-cd fuzzy-dedupe-pipeline-main
-
-# Fix 1: Rename init.py files to __init__.py
-mv sheets_sync/init.py sheets_sync/__init__.py
-mv dedupe_logic/init.py dedupe_logic/__init__.py
-
-# Fix 2: Also check if database needs __init__.py
-touch database/__init__.py
+# Empty file to make sheets_sync a Python package
