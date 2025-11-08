8a7f9554a12fc1c0ff967772a57c8d127d9ce56d
ClaytonS2013 <ClaytonS2013@gmail.com>
2025-11-07 23:37:58 -0600

    Update __init__.py

--- sheets_sync/__init__.py
+++ sheets_sync/__init__.py
@@ -4,15 +4,11 @@ This makes sheets_sync a proper Python package
 """
 
 from .sync import (
-    init_google_sheets_client,
-    init_supabase_client,
     sync_sheets_to_supabase,
     sync_supabase_to_sheets
 )
 
 __all__ = [
-    'init_google_sheets_client',
-    'init_supabase_client',
     'sync_sheets_to_supabase',
     'sync_supabase_to_sheets'
 ]
